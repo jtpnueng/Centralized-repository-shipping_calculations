10c7c04f6b83087e125bbe6331ee59ec8cbe1a35
jtpnueng <nueng_jatu_2543@hotmail.com>
2026-04-10 21:45:46 +0700

    Revert "added syntax"
    This reverts commit 51ce8b4308ddb06924e4ba0612f238c7d054f936.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -54,8 +54,3 @@ print ("dfdf")
 file_path = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/GkDzb7bWrtvGXdPOfk6CIg/Obesity-level-prediction-dataset.csv"
 data = pd.read_csv(file_path)
 data.head()
-
-
-
-#add-on some text
-print ("hi jatuporn")
